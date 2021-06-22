64fa87829c39e7eb334b5d275376478126e7bc2e
Felipe Riquelme <felipe33@gmail.com>
2021-06-21 22:38:09 -0400

    crea clase tipo producto

--- joyas/admin.py
+++ joyas/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from joyas.models import *
 
 # Register your models here.
+
+admin.site.register(tipo_producto)
--- joyas/models.py
+++ joyas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from djmoney.models.fields import MoneyField
 
 # Create your models here.
+
+class tipo_producto(models.Model):
+    id_tipo_producto = models.AutoField(primary_key=True)
+    nombre_tipo_producto = models.CharField(max_length=100)
+
+    def __str__(self):
+        txt = "{0}"
+        return txt.format(self.nombre_tipo_producto)
+

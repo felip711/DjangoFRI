35b474618c86070c1449cbc5e56c396e60043a67
Felipe Riquelme <felipe33@gmail.com>
2021-06-24 00:45:06 -0400

    html productos

--- proyectos/urls.py
+++ proyectos/urls.py
@@ -22,4 +22,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='Home'),
+    path('productos',views.productos,name='Productos'),
 ]
--- proyectos/views.py
+++ proyectos/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse('<h1>Página Principal</h1>')
 
+def productos(request):
+
+     context = {}
+     return render(request, 'joyas/productos.html', context)

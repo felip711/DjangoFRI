429780b679dd383fda953f67d5d8bb2bca149d3b
Felipe Riquelme <felipe33@gmail.com>
2021-06-24 00:37:43 -0400

    home

--- proyectos/urls.py
+++ proyectos/urls.py
@@ -21,5 +21,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('productos/', views.productos, name="productos"),
+    path('productos/', include('joyas.urls')),
+    path('', views.home,name='Home')),
 ]

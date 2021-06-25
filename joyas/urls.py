9119eeebdb8bfb2d846ef3d7b1697396174ceb2a
Felipe Riquelme <felipe33@gmail.com>
2021-06-24 23:29:39 -0400

    urls

--- joyas/urls.py
+++ joyas/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('productos/',views.productos,name='Productos'),
+    path('productos/',views.productos,name='productos'),
 ]

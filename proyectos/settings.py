9f08d9041aa5b133d37317c463c6b2819ab00bea
Felipe Riquelme <felipe33@gmail.com>
2021-06-27 18:34:01 -0400

    media root

--- proyectos/settings.py
+++ proyectos/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/imagenes/productos')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/imagenes/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

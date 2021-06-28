1c5da93dac04e6fc40cfe87e639cbed017d15c63
Felipe Riquelme <felipe33@gmail.com>
2021-06-28 00:50:39 -0400

    buscador productos

--- joyas/views.py
+++ joyas/views.py
@@ -5,4 +5,8 @@ from .models import *
 
 def productos(request):
      productos = producto.objects.all()
+
+     item_name = request.GET.get('item_name')
+     if item_name != '' and item_name is not None:
+          productos = productos.filter(title_icontains=item_name)
      return render(request, 'productos.html', {'productos':productos})

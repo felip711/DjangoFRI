a9754859ef5ed31847eaa86972b8543a3de1e824
Felipe Riquelme <felipe33@gmail.com>
2021-06-28 23:37:03 -0400

    filtro productos

--- joyas/views.py
+++ joyas/views.py
@@ -9,4 +9,5 @@ def productos(request):
      nombre_producto = request.GET.get('nombre_producto')
      if nombre_producto != '' and nombre_producto is not None:
           productos = productos.filter(nombre_producto__contains=nombre_producto)
+          print productos
      return render(request, 'productos.html', {'productos':productos})

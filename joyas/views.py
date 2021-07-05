19331cbd9c9a276c86aaaf0649a9843595494a08
Felipe Riquelme <felipe33@gmail.com>
2021-07-04 22:26:26 -0400

    paginador

--- joyas/views.py
+++ joyas/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from .models import *
+from django.core.paginator import Paginator
 # Create your views here.
 
 
 def productos(request):
      productos = producto.objects.all()
+
+     #Buscador
      nombre_producto = request.GET.get('nombre_producto')
      if nombre_producto != '' and nombre_producto is not None:
           productos = productos.filter(nombre_producto__contains=nombre_producto)
+     
+     #Paginador
+     paginador = Paginator(productos,10)
+     pagina = request.GET.get('page')
+     productos = paginador.get_page(pagina)
+
      return render(request, 'productos.html', {'productos':productos})
